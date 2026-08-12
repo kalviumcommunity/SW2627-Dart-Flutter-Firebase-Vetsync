5317444647a6a9ee159976c0bb42523f0dd065c4
nirbhayjakhar3196 <jakharnirbhay0000@gmail.com>
2026-08-12 23:30:16 +0530

    feat:Added home screen page

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('VetSync'),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome Doctor!🧿',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 16),
+
+            const Text(
+              'Your veterinary health records in one place.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 24),
+
+            ElevatedButton(onPressed: () {} , child: const Text('View Pets')),
+
+          ],
+        ),
+      ),
+    );
+    
+  }
+
+}
+
