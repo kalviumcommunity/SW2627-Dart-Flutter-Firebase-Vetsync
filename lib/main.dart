d4782cf7b83003324ba2b41ae15b5b8c706b8c27
nirbhayjakhar3196 <jakharnirbhay0000@gmail.com>
2026-08-19 11:57:29 +0530

    feat: Firebase setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:vetsync/screen/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:vetsync/firebase_options.dart';
 // import 'package:vetsync/screen/signup_screen.dart';
-// import 'package:vetsync/screen/home_screen.dart';
-// import 'package:vetsync/screen/login_screen.dart';
 
-void main() {
+
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
